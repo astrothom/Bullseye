0512bfd02e8f4d8faf0c5c93ebbdb2a115455c90
astrothom <thomas.pelfrene@protonmail.com>
2019-10-21 12:21:46 +0200

    Initial Commit

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Thomas Pelfrene on 21/10/2019.
+//  Copyright © 2019 Thomas Pelfrene. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
