73c4916221575d0b8f63ec15b02f7649e93ca54b
astrothom <thomas.pelfrene@protonmail.com>
2019-10-21 19:11:16 +0200

    learn about variables, conditions, methods, etc..

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -10,43 +10,83 @@ import SwiftUI
 
 struct ContentView: View {
   
-  @State var alertIsVisible: Bool = false
-  @State var whosthereIsVisible: Bool = false
+  @State var alertIsVisible = false
+  @State var sliderValue = 50.0
+  @State var targetValue = Int.random(in: 1...100)
+  
   
   var body: some View {
     VStack {
-      VStack {
-        Text("Welcome to my first app!")
-          .fontWeight(.semibold)
-          .foregroundColor(Color.green)
-        Button(action: {
-          print("Button pressed!")
-          self.alertIsVisible = true
-        }) {
-          Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
-        }
-        .alert(isPresented: $alertIsVisible) { () -> Alert in
-          return Alert(title: Text("Hello there!"),
-                       message: Text("This is my first pop-up."),
-                       dismissButton: .default(Text("Awesome!")))
-        }
+      Spacer()
+      
+      // Target row
+      HStack {
+        Text("Put the bullseye as close as you can to:")
+        Text("\(self.targetValue)")
       }
+      Spacer()
+      
+      // Slider row
+      HStack {
+        Text("1")
+        Slider(value: $sliderValue, in: 1...100)
+        Text("100")
+      }
+      Spacer()
+      
+      // Button row
       Button(action: {
-        self.whosthereIsVisible = true
+        print("Button pressed!")
+        self.alertIsVisible = true
       }) {
-        Text("Knock, Knock!")
+        Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
       }
-      .alert(isPresented: $whosthereIsVisible) { () -> Alert in
-        return Alert(title: Text("Who's There?"),
-                     message: Text("My funny joke."),
-                     dismissButton: .default(Text("Hahahaha!")))
+      .alert(isPresented: $alertIsVisible) { () -> Alert in
+        return Alert(title: Text("Hello there!"),
+                     message: Text(
+                      "The slider's value is: \(self.sliderValueRounded()).\n" +
+                      "You scored \(self.pointsForCurrentRound()) points this round."
+          ), dismissButton: .default(Text("Awesome!")))
       }
+      Spacer()
+      
+      // Score row
+      HStack {
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+        Text("Start over")
+        }
+        Spacer()
+        Text("Score:")
+        Text("999999")
+        Spacer()
+        Text("Round:")
+        Text("999")
+        Spacer()
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+        Text("Info")
+        }
+      }
+      .padding(.bottom, 20)
     }
   }
+  
+  func sliderValueRounded() -> Int {
+    return Int(self.sliderValue.rounded())
+  }
+  
+  func pointsForCurrentRound() -> Int {
+    return 100 - abs(self.targetValue - self.sliderValueRounded())
+  }
+  
 }
+
+/*
+ - first subtract the target value from the slider's value.
+ - Then, if the result is a negative number, multiply it by -1 to make it a positive number.
+ */
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView()
+    ContentView().previewLayout(.fixed(width: 812, height: 375))
   }
 }
